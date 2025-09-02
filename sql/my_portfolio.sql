e0ddc273ff40a83ef70716b6b48a4f1fb8713b07
Mharc Rigos <mcrei1018@gmail.com>
2025-09-02 16:26:53 +0800

    Last Day

--- sql/my_portfolio.sql
+++ sql/my_portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2025 at 10:49 AM
+-- Generation Time: Sep 02, 2025 at 10:23 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,7 +50,7 @@ CREATE TABLE `about` (
 
 INSERT INTO `about` (`id`, `user_id`, `name`, `email`, `degree`, `birthday`, `experience`, `address`, `company`, `phone`, `role`, `description`, `file_name`, `time_create`) VALUES
 (14, 57, 'test three', 'test_3@gmail.com', 'BSPSCYH', '2004-04-05', 'None', '19 J. Bernal St. Sta. Mesa', 'FUGRO', '0915-451-4596', 'Cook', 'Let me cook', 'MicrosoftTeams-image.png', '2025-08-22 05:50:32'),
-(15, 52, 'test two', 'test_2@gmail.com', 'BSCPE', '2004-10-18', 'Website Tester/QA', 'C. Santos St.', 'CITYSTYLE', '0915-451-4596', 'D', 'A passionate and dedicated individual with a strong interest in technology, programming, and problem-solving. Continuously learning and developing skills in software development, with a focus on building responsive, user-friendly websites and efficient sof', 'background_image.png', '2025-08-19 07:59:14'),
+(15, 52, 'test two', 'test_2@gmail.com', 'BSCPE', '2004-10-18', 'Website Tester/QA', 'C. Santos St.', 'CITYSTYLE', '0915-451-4596', 'D', 'A passionate and dedicated individual with a strong interest in technology, programming, and problem-solving. Continuously learning and developing skills in software development, with a focus on building responsive, user-friendly websites and efficient sof', 'about_picture.JPG', '2025-08-29 08:34:19'),
 (16, 58, 'test four', 'test_4@gmail.com', 'BSCPE', '2025-07-29', 'hello', '21', 'CITYSTYLE', '12341234', '', '', NULL, '2025-08-08 05:50:39'),
 (17, 65, 'test six', 'test_6@gmail.com', 'try', '2025-07-28', 'try', 'tyr', 'try', '123412341234', '', '', NULL, '2025-08-11 01:28:20'),
 (33, 78, 'test seven', 'test_7@gmail.com', 'BSPSCYH', '2025-07-29', 'Website Tester/QA', 'C. Santos St.', 'CITYSTYLE', '0915-451-4596', 'Future Prof', 'yiui', 'Screenshot 2025-08-15 121544.png', '2025-08-18 07:18:06'),
@@ -76,7 +76,8 @@ CREATE TABLE `contacts` (
 
 INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`) VALUES
 (14, 'dadad', 'test_2@gmail.com', 'qw', 'qwer'),
-(15, 'dadad', 'test_2@gmail.com', 'qw', 'qwer');
+(15, 'dadad', 'test_2@gmail.com', 'qw', 'qwer'),
+(16, 'dadad', 'test_2@gmail.com', 'qw', 'asdasd');
 
 -- --------------------------------------------------------
 
@@ -203,7 +204,96 @@ INSERT INTO `logs` (`id`, `user_id`, `action`, `timestamp`) VALUES
 (113, 52, 'Created a Skill', '2025-08-27 08:25:11'),
 (114, 52, 'Created a Skill', '2025-08-27 08:25:31'),
 (115, 52, 'Created a Skill', '2025-08-27 08:26:36'),
-(116, 52, 'Created a Skill', '2025-08-27 08:33:48');
+(116, 52, 'Created a Skill', '2025-08-27 08:33:48'),
+(117, 52, 'Logged In', '2025-08-27 23:01:42'),
+(118, 52, 'Created a Project', '2025-08-27 23:11:05'),
+(119, 52, 'Deleted a Project', '2025-08-27 23:11:30'),
+(120, 52, 'Created a Project', '2025-08-27 23:11:37'),
+(121, 52, 'Deleted a Project', '2025-08-27 23:12:23'),
+(122, 52, 'Created a Project', '2025-08-27 23:13:40'),
+(123, 52, 'Deleted a Project', '2025-08-27 23:14:11'),
+(124, 52, 'Created a Skill', '2025-08-27 23:37:29'),
+(125, 52, 'Edited a Skill', '2025-08-27 23:37:36'),
+(126, 52, 'Deleted a Skill', '2025-08-27 23:40:09'),
+(127, 52, 'Created a Skill', '2025-08-27 23:40:14'),
+(128, 52, 'Edited a Skill', '2025-08-27 23:40:21'),
+(129, 52, 'Deleted a Skill', '2025-08-27 23:41:25'),
+(130, 52, 'Created a Skill', '2025-08-27 23:41:28'),
+(131, 52, 'Edited a Skill', '2025-08-27 23:41:35'),
+(132, 52, 'Deleted a Skill', '2025-08-27 23:42:05'),
+(133, 52, 'Created a Skill', '2025-08-27 23:42:10'),
+(134, 52, 'Edited a Skill', '2025-08-27 23:42:19'),
+(135, 52, 'Deleted a Skill', '2025-08-27 23:43:49'),
+(136, 52, 'Created a Skill', '2025-08-27 23:43:54'),
+(137, 52, 'Created a Skill', '2025-08-28 00:02:10'),
+(138, 52, 'Edited a Skill', '2025-08-28 00:10:00'),
+(139, 52, 'Edited a Skill', '2025-08-28 00:10:09'),
+(140, 52, 'Edited a Skill', '2025-08-28 00:11:05'),
+(141, 52, 'Deleted a Skill', '2025-08-28 00:12:51'),
+(142, 52, 'Created a Skill', '2025-08-28 00:12:55'),
+(143, 52, 'Edited a Skill', '2025-08-28 00:32:01'),
+(144, 52, 'Edited a Skill', '2025-08-28 00:43:07'),
+(145, 52, 'Edited a Skill', '2025-08-28 00:43:14'),
+(146, 52, 'Edited a Skill', '2025-08-28 00:43:20'),
+(147, 52, 'Edited a Skill', '2025-08-28 00:44:35'),
+(148, 52, 'Edited a Skill', '2025-08-28 00:44:42'),
+(149, 52, 'Created a Skill', '2025-08-28 00:50:19'),
+(150, 52, 'Edited a Project', '2025-08-28 06:05:02'),
+(151, 52, 'Created a Project', '2025-08-28 06:14:13'),
+(152, 52, 'Edited a Project', '2025-08-28 06:15:13'),
+(153, 52, 'Deleted a Project', '2025-08-28 06:15:45'),
+(154, 52, 'Created a Project', '2025-08-28 06:15:50'),
+(155, 52, 'Edited a Project', '2025-08-28 06:15:56'),
+(156, 52, 'Deleted a Project', '2025-08-28 06:22:24'),
+(157, 52, 'Edited a Project', '2025-08-28 06:22:31'),
+(158, 52, 'Deleted a Project', '2025-08-28 06:24:12'),
+(159, 52, 'Created a Project', '2025-08-28 06:24:24'),
+(160, 52, 'Created a Project', '2025-08-28 06:24:30'),
+(161, 52, 'Created a Project', '2025-08-28 06:24:36'),
+(162, 52, 'Created a Project', '2025-08-28 06:29:32'),
+(163, 52, 'Uploaded a Picture in Projects', '2025-08-28 06:30:50'),
+(164, 52, 'Edited and Uploaded a Picture in Project', '2025-08-28 06:31:05'),
+(165, 52, 'Edited and Uploaded a Picture in Project', '2025-08-28 06:31:21'),
+(166, 52, 'Created a Project', '2025-08-28 06:37:31'),
+(167, 52, 'Created a Project', '2025-08-28 06:37:39'),
+(168, 52, 'Created a Project', '2025-08-28 06:37:45'),
+(169, 52, 'Created a Project', '2025-08-28 06:38:10'),
+(170, 52, 'Edited a Project', '2025-08-28 06:38:41'),
+(171, 52, 'Edited a Project', '2025-08-28 06:38:45'),
+(172, 52, 'Edited and Uploaded a Picture in Project', '2025-08-28 06:49:38'),
+(173, 52, 'Uploaded a Picture in Projects', '2025-08-28 06:50:45'),
+(174, 52, 'Uploaded a Picture in Projects', '2025-08-28 06:51:05'),
+(175, 52, 'Edited a Project', '2025-08-28 06:51:32'),
+(176, 52, 'Edited and Uploaded a Picture in Project', '2025-08-28 06:51:43'),
+(177, 52, 'Logged In', '2025-08-28 23:45:09'),
+(178, 52, 'Logged Out', '2025-08-28 23:55:55'),
+(179, 52, 'Logged In', '2025-08-29 00:02:10'),
+(180, 52, 'Uploaded a Picture', '2025-08-29 02:28:13'),
+(181, 52, 'Uploaded a Picture', '2025-08-29 02:45:50'),
+(182, 52, 'Uploaded a Picture', '2025-08-29 02:47:20'),
+(183, 52, 'Logged Out', '2025-08-29 03:06:37'),
+(184, 78, 'Logged In', '2025-08-29 03:06:44'),
+(185, 78, 'Logged Out', '2025-08-29 03:06:48'),
+(186, 77, 'Logged In', '2025-08-29 03:06:53'),
+(187, 77, 'Logged Out', '2025-08-29 03:06:57'),
+(188, 64, 'Logged In', '2025-08-29 03:07:29'),
+(189, 64, 'Inserted About', '2025-08-29 03:26:02'),
+(190, 64, 'Inserted About', '2025-08-29 03:29:17'),
+(191, 64, 'Inserted About', '2025-08-29 03:31:56'),
+(192, 64, 'Inserted About', '2025-08-29 03:33:35'),
+(193, 64, 'Inserted About', '2025-08-29 03:41:51'),
+(194, 64, 'Uploaded a Picture', '2025-08-29 03:43:56'),
+(195, 64, 'Logged Out', '2025-08-29 03:50:46'),
+(196, 52, 'Logged In', '2025-08-29 03:50:53'),
+(197, 52, 'Uploaded a Picture', '2025-08-29 03:51:43'),
+(198, 52, 'Logged Out', '2025-08-29 03:57:53'),
+(199, 64, 'Logged In', '2025-08-29 03:58:13'),
+(200, 64, 'Logged Out', '2025-08-29 05:32:37'),
+(201, 52, 'Logged In', '2025-08-29 05:39:08'),
+(202, 52, 'Uploaded a Picture', '2025-08-29 08:34:19'),
+(203, 52, 'Logged Out', '2025-08-29 08:56:28'),
+(204, 52, 'Logged In', '2025-08-29 08:59:07'),
+(205, 52, 'Logged In', '2025-09-01 23:56:44');
 
 -- --------------------------------------------------------
 
@@ -230,9 +320,10 @@ INSERT INTO `projects` (`id`, `name`, `description`, `status`, `user_id`, `file_
 (170, 'test 1', 'try', 'Start', 58, NULL, '2025-08-08 04:08:54'),
 (173, 'test 1', 'asdfsdafasdfasdfsadfasdf', 'Start', 65, NULL, '2025-08-11 01:28:48'),
 (185, 'weather system', '123', 'In Progress', 57, NULL, '2025-08-13 08:03:57'),
-(289, '5', 'qwer', 'Start', 52, 'Screenshot 2025-08-06 074958.png', '2025-08-20 07:35:38'),
-(290, '6', 'qwer', 'Start', 52, 'hide.png', '2025-08-20 07:35:43'),
-(295, '8', 'qwerty', 'Done', 52, 'MicrosoftTeams-image.png', '2025-08-22 07:25:43');
+(305, '1', 'q', 'Start', 52, NULL, '2025-08-28 06:37:31'),
+(306, '2', 'w', 'In Progress', 52, NULL, '2025-08-28 06:37:39'),
+(307, '3', 'e', 'Done', 52, NULL, '2025-08-28 06:37:45'),
+(308, '4', 'r23', 'In Progress', 52, 'images.jpg', '2025-08-28 06:38:10');
 
 -- --------------------------------------------------------
 
@@ -258,7 +349,10 @@ INSERT INTO `skills` (`id`, `name`, `type`, `rate`, `user_id`) VALUES
 (83, 'Programming', 'Soft', '3 - Satisfactory', 65),
 (100, 'Teamwork', 'Knowledge-based', 'Satisfactory', 57),
 (101, 'Programming', 'Knowledge-based', 'Very Good', 57),
-(146, 'Programming', 'Soft', 'Satisfactory', 52);
+(146, 'Programming', 'Hard', 'Good', 52),
+(152, 'Communication', 'Soft', 'Satisfactory', 52),
+(153, 'Math', 'Hard', 'Good', 52),
+(154, 'Reading', 'Soft', 'Good', 52);
 
 -- --------------------------------------------------------
 
@@ -336,37 +430,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `about`
 --
 ALTER TABLE `about`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=206;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=296;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=309;
 
 --
 -- AUTO_INCREMENT for table `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
